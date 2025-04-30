9fc0450b6dc7bbbb990b82c5b3c0eb7309904808
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2025-04-29 21:23:46 -0600

    install the package for the navigation

---
+++ screens/AllExpenses.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function AllExpenses() {
+    return (
+        <View>
+            <Text>All expenses Screen</Text>
+        </View>
+    )
+}
+
+export default AllExpenses;
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function RecentExpenses() {
+    return (
+        <View>
+            <Text>Recent expenses Screen</Text>
+        </View>
+    )
+}
+
+export default RecentExpenses;

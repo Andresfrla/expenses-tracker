4799a58f92f007c9502043f269070e9081558043
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2025-05-02 17:14:01 -0600

    Create the structure for the logic on the form

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,11 +1,42 @@
 import { TextInput, View } from "react-native";
+import Input from "./Input";
 
-function ExpenseForm () {
-    return(
-        <View>
-            
-        </View>
-    )
+function ExpenseForm() {
+    function amountChangeHandler() {
+
+    }
+
+    function dateChangeHandler() {
+
+    }
+
+    function descriptionChangeHandler() {
+
+    }
+
+  return (
+    <View>
+      <Input
+        label="Amount"
+        textInputConfig={{
+          keyboardType: "decimal-pad",
+          onChangeText: amountChangeHandler(),
+        }}
+      />
+      <Input label="Date" textInputConfig={{
+        placeholder: "YYYY-MM-DD",
+        maxLength: 10,
+        onChangeText: dateChangeHandler(),
+      }} />
+      <Input label="Description" 
+        textInputConfig={{
+          multiline: true,
+          autoCorrect: false,
+          onChangeText: descriptionChangeHandler(),
+        }}
+      />
+    </View>
+  );
 }
 
 export default ExpenseForm;

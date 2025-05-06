1e8290d1a4c3348f3820d80fdbf6b94833ffcf95
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2025-05-05 19:50:02 -0600

    Fetch the data correctly

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,15 +1,16 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../util/date";
 import { fetchExpenses } from "../util/http";
 
 function RecentExpenses() {
-  const expensesCtx = useContext(ExpensesContext);
+  const expensesCtx = useContext(ExpensesContext); 
 
   useEffect(() => {
     async function getExpenses() {
-      await fetchExpenses();
+      const expenses = await fetchExpenses();
+      expensesCtx.setExpenses(expenses)
     }
 
     getExpenses();
--- store/expenses-context.js
+++ store/expenses-context.js
@@ -1,65 +1,9 @@
 import { createContext, useReducer } from "react";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    description: "A pair of shoes",
-    amount: 59.99,
-    date: new Date("2025-04-28"),
-  },
-  {
-    id: "e2",
-    description: "A pair of pants",
-    amount: 39.99,
-    date: new Date("2025-01-13"),
-  },
-  {
-    id: "e3",
-    description: "A book",
-    amount: 29.99,
-    date: new Date("2025-01-14"),
-  },
-  {
-    id: "e4",
-    description: "A new TV",
-    amount: 799.99,
-    date: new Date("2025-01-15"),
-  },
-  {
-    id: "e5",
-    description: "A new laptop",
-    amount: 1299.99,
-    date: new Date("2025-01-16"),
-  },
-  {
-    id: "e6",
-    description: "A new phone",
-    amount: 999.99,
-    date: new Date("2025-01-17"),
-  },
-  {
-    id: "e7",
-    description: "A new tablet",
-    amount: 499.99,
-    date: new Date("2025-01-18"),
-  },
-  {
-    id: "e8",
-    description: "A new watch",
-    amount: 199.99,
-    date: new Date("2025-01-19"),
-  },
-  {
-    id: "e9",
-    description: "A new camera",
-    amount: 899.99,
-    date: new Date("2025-01-20"),
-  },
-];
-
 export const ExpensesContext = createContext({
   expenses: [],
   addExpense: ({ description, amount, date }) => {},
+  setExpenses: (expenses) => {},
   deleteExpense: (id) => {},
   updateExpense: (id, { description, amount, date }) => {},
 });
@@ -69,6 +13,8 @@ function expensesReducer(state, action) {
     case "ADD":
       const id = new Date().toString() + Math.random().toString();
       return [{ ...action.payload, id: id }, ...state];
+    case "SET":
+      return action.payload; 
     case "UPDATE":
       const updatableExpenseIndex = state.findIndex(
         (expense) => expense.id === action.payload.id
@@ -86,10 +32,14 @@ function expensesReducer(state, action) {
 }
 
 export const ExpenseContextProvider = ({ children }) => {
-  const [expensesState, dispatch] = useReducer(expensesReducer, DUMMY_EXPENSES);
+  const [expensesState, dispatch] = useReducer(expensesReducer, []);
 
   function addExpense(expenseData) {
     dispatch({ type: "ADD", payload: expenseData });
+  }
+
+  function setExpenses(expenses){
+    dispatch({type: "SET", payload: expenses})
   }
 
   function deleteExpense(id) {
@@ -100,12 +50,17 @@ export const ExpenseContextProvider = ({ children }) => {
     dispatch({ type: "UPDATE", payload: { id: id, data: expenseData } });
   }
 
-    const value = {
-        expenses: expensesState,
-        addExpense: addExpense,
-        deleteExpense: deleteExpense,
-        updateExpense: updateExpense,
-    };
+  const value = {
+    expenses: expensesState,
+    setExpenses: setExpenses,
+    addExpense: addExpense,
+    deleteExpense: deleteExpense,
+    updateExpense: updateExpense,
+  };
 
-  return <ExpensesContext.Provider value={value}>{children}</ExpensesContext.Provider>;
+  return (
+    <ExpensesContext.Provider value={value}>
+      {children}
+    </ExpensesContext.Provider>
+  );
 };

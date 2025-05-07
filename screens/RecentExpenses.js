bd76d5dcf33d6fab002da546332e7171be0c767d
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2025-05-06 20:33:57 -0600

    Set a state for the error

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -4,26 +4,35 @@ import { ExpensesContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../util/date";
 import { fetchExpenses } from "../util/http";
 import LoadingOverlay from "../components/UI/LoadingOverlay";
+import ErrorOverlay from "../components/UI/ErrorOverlay";
 
 function RecentExpenses() {
   const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState("");
+
   const expensesCtx = useContext(ExpensesContext);
 
   useEffect(() => {
     async function getExpenses() {
       setIsFetching(true);
-      const expenses = await fetchExpenses();
-      setIsFetching(false)
-      expensesCtx.setExpenses(expenses);
+      try {
+        const expenses = await fetchExpenses();
+        expensesCtx.setExpenses(expenses);
+      } catch (error) {
+        setError("Could not fetch expenses!");
+      }
+      setIsFetching(false);
     }
 
     getExpenses();
   }, []);
 
-  if(isFetching){
-    return (
-      <LoadingOverlay />
-    )
+  if(error && !isFetching){
+    return <ErrorOverlay message={error}/>
+  }
+
+  if (isFetching) {
+    return <LoadingOverlay />;
   }
 
   function stripTime(date) {

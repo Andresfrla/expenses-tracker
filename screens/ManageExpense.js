3a4c8c5c91600b742fd27f0241dbbae7007bfd2e
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2025-05-06 20:14:43 -0600

    Add loading spinners on the fetch of the data

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,12 +1,14 @@
-import { useContext, useLayoutEffect } from "react";
+import { useContext, useLayoutEffect, useState } from "react";
 import { StyleSheet, TextInput, View } from "react-native";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
 import { ExpensesContext } from "../store/expenses-context";
 import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 import { storeExpense, updateExpense, deleteExpense } from "../util/http";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 
 function ManageExpense({ route, navigation }) {
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const expensesCtx = useContext(ExpensesContext);
 
   const editedExpenseId = route.params?.expenseId;
@@ -23,7 +25,9 @@ function ManageExpense({ route, navigation }) {
   }, [editedExpenseId, isEditing]);
 
   async function deleteExpenseHandler() {
+    setIsSubmitting(true);
     await deleteExpense(editedExpenseId);
+    // setIsSubmitting(false);
     expensesCtx.deleteExpense(editedExpenseId);
     navigation.goBack();
   }
@@ -33,6 +37,7 @@ function ManageExpense({ route, navigation }) {
   }
 
   async function confirmHandler(expenseData) {
+    setIsSubmitting(true);
     if (isEditing) {
       expensesCtx.updateExpense(editedExpenseId, expenseData);
       await updateExpense(editedExpenseId, expenseData);
@@ -41,6 +46,10 @@ function ManageExpense({ route, navigation }) {
       expensesCtx.addExpense({ ...expenseData, id: id });
     }
     navigation.goBack();
+  }
+
+  if (isSubmitting) {
+    return <LoadingOverlay />;
   }
 
   return (
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -3,18 +3,28 @@ import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../util/date";
 import { fetchExpenses } from "../util/http";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 
 function RecentExpenses() {
-  const expensesCtx = useContext(ExpensesContext); 
+  const [isFetching, setIsFetching] = useState(true);
+  const expensesCtx = useContext(ExpensesContext);
 
   useEffect(() => {
     async function getExpenses() {
+      setIsFetching(true);
       const expenses = await fetchExpenses();
-      expensesCtx.setExpenses(expenses)
+      setIsFetching(false)
+      expensesCtx.setExpenses(expenses);
     }
 
     getExpenses();
   }, []);
+
+  if(isFetching){
+    return (
+      <LoadingOverlay />
+    )
+  }
 
   function stripTime(date) {
     return new Date(date.getFullYear(), date.getMonth(), date.getDate());
